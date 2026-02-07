11c45618fcbedfcf1e9407bc5f820a37edc2386e
Angie <itsreallyrainy@gmail.com>
2026-02-07 13:00:50 -0500

    rudder debugging and test

--- sailbench/foils/basic_rudder.py
+++ sailbench/foils/basic_rudder.py
@@ -30,23 +30,26 @@ class BasicRudder(Foil):
             np.ndarray: Returns X and Y forces in newtons (within rudder frame)
 
         """
-        # get angle of attack in radians
-        # alpha_rad = np.radians(angle_input)
-        global_track_vector = np.ndarray(state.u, state.v)
-        local_track_vector = utils.global_to_local(global_track_vector)
+        # get local track vector
+        global_track_vector = np.array([[state.u],
+                                [state.v]])
+        local_track_vector = utils.global_to_local(global_track_vector, state.psi)
+        
 
 
         #Rotation matrix: local to rudder frame
         rotate_into_rudder = np.array([
-        [np.cosine(np.radians(angle_input)), np.sine(np.radians(angle_input))],
-        [-1* np.sine(np.radians(angle_input)), np.cosine(np.radians(angle_input))],
+        [np.cos(np.radians(angle_input)), -1 * np.sin(np.radians(angle_input))],
+        [np.sin(np.radians(angle_input)), np.cos(np.radians(angle_input))],
         ])
 
-        rudder_frame_track = local_track_vector @ rotate_into_rudder
+        rudder_frame_track = rotate_into_rudder @ local_track_vector
 
-
+        #angle of attack is angle of vector of track in rudder frame
+        angle_of_track = np.arctan2(rudder_frame_track[1, 0] , rudder_frame_track[0, 0])
+        
         # get lift and drag coefficients
-        cl, cd = self.cl_cd(alpha_rad, re=self.p.get("re", 1e5))
+        cl, cd = self.cl_cd(angle_of_track, re=self.p.get("re", 1e5))
 
         # compute dynamic pressure
         rho = self.p.get("water_density", 1000.0)  # kg/m^3
@@ -57,13 +60,25 @@ class BasicRudder(Foil):
 
         # compute forces
         s = self.p.get("area", 1.0)  # m^2
-        cl, cd = self.cl_cd(np.radians(np.arctan2(rudder_frame_track[1], rudder_frame_track[0])), re=self.p.get("re", 1e5)) 
-        lift = cl * q * s
+    
         drag = cd * q * s
+        lift = cl * q * s
 
-        # return forces in rudder frame (X forward, Y starboard)
-        #TODO : convert back to local frame
+        # return forces in fluid frame (X forward, Y starboard)
         fx = -drag
-        fy = -lift
+        fy = lift
 
-        return np.array([fx, fy])
+        rotate_into_rudder = np.array([
+            [np.cos(angle_of_track), np.sin(angle_of_track)],
+        [-1* np.sin(angle_of_track), np.cos(angle_of_track)],
+        ])
+        # convert rudder frame to local frame
+        rotate_into_local = np.array([
+        [np.cos(np.radians(angle_input)), np.sin(np.radians(angle_input))],
+        [-1* np.sin(np.radians(angle_input)), np.cos(np.radians(angle_input))],
+        ])
+
+        # rotate lift and drag fluid --> rudder --> local frame
+        f_prime = (rotate_into_local) @ (rotate_into_rudder) @ np.array([[fx],[fy]])
+
+        return np.asarray([f_prime[0,0], f_prime[1,0]])
--- test/conftest.py
+++ test/conftest.py
@@ -7,11 +7,12 @@ import pytest
 import yaml
 
 from sailbench.foils.basic_keel import BasicKeel
+from sailbench.foils.basic_rudder import BasicRudder
 
 
 @pytest.fixture
 def config() -> dict[str, Any]:
-    """Load the basic sailboat configuration for testing."""
+    """Load the basic sailboat congifiguration for testing."""
     with Path("configs/basic_sailbot.yaml").open() as file:
         return dict(yaml.safe_load(file))
 
@@ -21,3 +22,9 @@ def keel(config: dict[str, Any]) -> BasicKeel:
     """Generate a BasicKeel instance for testing."""
     keel_cfg = config["keel"]
     return BasicKeel(keel_cfg)
+
+@pytest.fixture
+def rudder(config: dict[str, Any]) -> BasicRudder:
+    """Generate a BasicRudder instance for testing."""
+    rudder_cfg = config["rudder"]
+    return BasicRudder(rudder_cfg)
